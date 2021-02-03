1b4e1d80c658c2c3bb7f6ac290005eb920fc9105
Mikio Nishimura <mikio_westvil@yahoo.ca>
2021-02-03 14:11:23 -0500

    first commit

---
+++ config/prod.exs
@@ -0,0 +1,6 @@
+use Mix.Config
+
+
+# Finally import the config/prod.secret.exs which loads secrets
+# and configuration from environment variables.
+import_config "prod.secret.exs"
---
+++ lib/info_sys/application.ex
@@ -0,0 +1,26 @@
+defmodule InfoSys.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: InfoSys.Worker.start_link(arg)
+      # {InfoSys.Worker, arg}
+      #{InfoSys.Counter, 5},   # new counter worker
+      #Supervisor.child_spec({InfoSys.Counter, 5}, restart: :temporary)
+      InfoSys.Cache,
+      {Task.Supervisor, name: InfoSys.TaskSupervisor},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: InfoSys.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+
+end
