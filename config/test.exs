fd4e288c98eadd12bfd5cbda59cb0e4e57a215ef
Mikio Nishimura <mikio_westvil@yahoo.ca>
2021-02-03 19:44:50 -0500

    Chp13 Testing p288

--- config/test.exs
+++ config/test.exs
@@ -4,3 +4,7 @@ use Mix.Config
 # Print only warnings and errors during test
 config :logger, level: :warn
 
+config :info_sys, :wolfram,
+    app_id: "1234",
+    http_client: InfoSys.Test.HTTPClient
+
